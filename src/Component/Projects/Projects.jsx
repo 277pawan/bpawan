9f35e1aa9f3f8fd33ab21fdf100051a58b444895
Pawan Bisht <124587748+277pawan@users.noreply.github.com>
2025-04-03 15:51:30 +0530

    Update Projects.jsx

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -1,15 +1,15 @@
 import "./Projects.css";
-import proj1 from "../../Assets/Lap1.png";
-import proj2 from "../../Assets/Lap2.png";
-import proj3 from "../../Assets/Lap3.png";
-import proj4 from "../../Assets/Lap4.png";
+// import proj1 from "../../Assets/Lap1.png";
+// import proj2 from "../../Assets/Lap2.png";
+// import proj3 from "../../Assets/Lap3.png";
+// import proj4 from "../../Assets/Lap4.png";
 import One from "../Gallery/One.tsx";
 function Projects() {
   return (
     <>
       <div id="projects" className="bg-[#0a0a0a] text-white">
         <div className="projectbox1">
-          <div className="text-lg md:text-4xl mb-4 text-center text-black dark:text-white max-w-full uppercase tracking-[0.5rem] font-bold">
+          <div className="text-lg md:text-4xl mb-4 text-center text-white dark:text-white max-w-full uppercase tracking-[0.5rem] font-bold">
             Projects
           </div>{" "}
           <hr
