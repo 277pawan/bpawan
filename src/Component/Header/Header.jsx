e686e5a0688a238b0f42a7314bc104f64600623d
277pawan <bpawan277@gmail.com>
2025-07-08 09:12:56 +0530

    rem: removed Sparkles component

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
     "Performance.",
     "Scalability.",
     "Success.",
-    // "Backend Architect",
+    "Backend Architect",
     "Tech Explorer",
   ];
 
@@ -77,15 +77,6 @@ function Header() {
           <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-
-          <SparklesCore
-            background="transparent"
-            minSize={0.4}
-            maxSize={1}
-            particleDensity={1200}
-            className="w-full h-full"
-            particleColor="#FFFFFF"
-          />
 
           <div className="flex justify-center items-center">
             <div
